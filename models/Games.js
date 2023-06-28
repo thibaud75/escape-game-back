a2a1b6465849fa4dd831f61a5274665c0e83bac3
Marc-E-B <marc.buran@outlook.fr>
2023-06-28 09:28:07 +0200

    add petiteDesc

--- models/Games.js
+++ models/Games.js
@@ -3,6 +3,7 @@ const uniqueValidator = require("mongoose-unique-validator");
 
 const gamesSchema = mongoose.Schema({
   name: { type: String, required: true, unique: true },
+  petitDesc:{ type: String, required: true },
   description: { type: String, required: true },
   price: { type: Number, required: true },
   capacity: { type: [Number], required: true },

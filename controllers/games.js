2f6ce7eaeaa8fa720129fb07de1a75aa92c3909c
thibaud75 <thibaud.delvert@gmail.com>
2023-06-28 12:55:52 +0200

    ajout des dispo

--- controllers/games.js
+++ controllers/games.js
@@ -21,3 +21,30 @@ exports.getOneGame = (req, res, next) => {
       res.status(400).json({ error: error });
     });
 };
+
+exports.saveDate = (req, res) => {
+  const { dates } = req.body;
+
+  // Vérifiez si les données requises sont présentes
+  if (!dates || !Array.isArray(dates)) {
+    return res.status(400).json({ message: "Données manquantes ou invalides" });
+  }
+
+  // Créez un nouvel objet Games avec les dates fournies
+  const myData = new Games({ disponibility: dates });
+
+  // Enregistrez l'objet dans la base de données
+  myData
+    .save()
+    .then(() => {
+      res.status(200).json({ message: "Données enregistrées avec succès" });
+    })
+    .catch((error) => {
+      res
+        .status(500)
+        .json({
+          message: "Erreur lors de l'enregistrement des données",
+          error,
+        });
+    });
+};
--- routes/games.js
+++ routes/games.js
@@ -5,6 +5,7 @@ const gamesCtrl = require("../controllers/games");
 
 router.get("/", gamesCtrl.getAllGames);
 router.get("/:id", gamesCtrl.getOneGame);
+router.post("/date", gamesCtrl.saveDate);
 // router.post('/:id/booking', gamesCtrl.bookingGame)
 
 module.exports = router;

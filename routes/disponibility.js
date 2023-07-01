072d91c8a87d7169c9d387a26c7a6455fdd07da6
thibaud75 <thibaud.delvert@gmail.com>
2023-07-01 13:37:39 +0200

    middleware auth

--- routes/disponibility.js
+++ routes/disponibility.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../middleware/auth");
 
 const disponibilityCtrl = require("../controllers/disponibility");
 
-router.post("/reserveform", disponibilityCtrl.reserveForm);
+router.post("/reserveform", auth, disponibilityCtrl.reserveForm);
 router.get("/history/:id", disponibilityCtrl.history);
 router.get("/getdates/:id", disponibilityCtrl.getDates);
-router.get("/getalldispo", disponibilityCtrl.getAllDispo)
-
+router.get("/getalldispo", disponibilityCtrl.getAllDispo);
 
 module.exports = router;
